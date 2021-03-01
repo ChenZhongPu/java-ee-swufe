1af738b82c05ebb000db5c03e45080d4660829c8
Zhongpu Chen <chenloveit@gmail.com>
2021-03-01 17:14:19 +0800

    a simple http server

---
+++ 01_intro_to_javaee/README.md
@@ -0,0 +1,7 @@
+# HTML
+[W3school](https://www.w3schools.com/html/default.asp) is a good site to learn HTML, CSS and JavaScript.
+
+# HTTP
+Most of the conversations held on the web between clients and servers are held using the HTTP protocol, which allows for simple request and response conversations. The client sends an HTTP request, and the server answers with an HTTP response.
+
+For more technique details, you may refer to [HTTP: The Definitive Guide](https://book.douban.com/subject/1440226/).
---
+++ 01_intro_to_javaee/http/src/main/java/Server.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Handler extends Thread {
+    Socket socket;
+
+    public Handler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (InputStream input = this.socket.getInputStream()) {
+            try (OutputStream output = this.socket.getOutputStream()) {
+                handle(input, output);
+            }
+        } catch (Exception e) {
+            try {
+                this.socket.close();
+            } catch (IOException ioe) {
+            }
+            System.out.println("client disconnected");
+        }
+    }
+
+    private void handle(InputStream input, OutputStream output) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+        // 读取HTTP请求:
+        boolean requestOk = false;
+        String first = reader.readLine();
+        if (first.startsWith("GET / HTTP/1.")) {
+            requestOk = true;
+        }
+        while (true) {
+            String header = reader.readLine();
+            if (header.isEmpty()) { // When founding a empty line, HTTP Header has been read
+                break;
+            }
+            System.out.println(header);
+        }
+        System.out.println(requestOk ? "Response OK" : "Response Error");
+        if (!requestOk) {
+            // error
+            writer.write("HTTP/1.0 404 Not Found\r\n");
+            writer.write("Content-Length: 0\r\n");
+            writer.write("\r\n");
+            writer.flush();
+        } else {
+            // success
+            String data = "<html><body><h1>Hello, world!</h1></body></html>";
+            int length = data.getBytes().length;
+            writer.write("HTTP/1.0 200 OK\r\n");
+            writer.write("Connection: close\r\n");
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: " + length + "\r\n");
+            writer.write("\r\n"); // new line between header and html
+            writer.write(data);
+            writer.flush();
+        }
+    }
+}
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(8888);
+        System.out.println("Server started...");
+        while (true) {
+            Socket socket = ss.accept();
+            System.out.println("connected from " + socket.getRemoteSocketAddress());
+            Thread t = new Handler(socket);
+            t.start();
+        }
+    }
+}

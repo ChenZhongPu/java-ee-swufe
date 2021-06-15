12d26f454278e116453b78cf89cf5062d003a08b
Zhongpu Chen <chenloveit@gmail.com>
2021-06-15 17:29:09 +0800

    update REST

--- restdemo/src/main/java/com/example/restdemo/PersonResource.java
+++ restdemo/src/main/java/com/example/restdemo/PersonResource.java
@@ -4,11 +4,10 @@ import com.example.restdemo.model.Person;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/person")
+@Path("/persons")
 public class PersonResource {
     @POST
     @Produces("text/plain")
-    @Path("/create")
     public String signup(@FormParam("name") String name,
                          @FormParam("pwd") String password) {
         Person person = new Person(name, password);

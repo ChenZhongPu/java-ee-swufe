1eee59bf8c146cf21ab8afd478b9c35828576ff3
CHEN zhongpu <chenloveit@gmail.com>
2021-12-31 11:14:55 +0800

    remove jakarta ee version

---
+++ ch2/localtime/src/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate time = LocalDate.parse("2008-08-08");
+        System.out.println(time);
+    }
+}

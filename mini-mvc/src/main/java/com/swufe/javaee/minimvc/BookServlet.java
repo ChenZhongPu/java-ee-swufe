f1d78e3275480626a1eb5df000457d051a40c0c7
CHEN zhongpu <chenloveit@gmail.com>
2022-01-02 22:31:04 +0800

    second version of mini-mvc

--- mini-mvc/src/main/java/com/swufe/javaee/minimvc/BookServlet.java
+++ mini-mvc/src/main/java/com/swufe/javaee/minimvc/BookServlet.java
@@ -5,6 +5,7 @@ import com.swufe.javaee.minimvc.model.Recommendation;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.awt.print.Book;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -16,8 +17,12 @@ public class BookServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
         String book = request.getParameter("books");
+        List<String> books = new Recommendation().getBooks(book);
         out.println("Book Selection:");
         out.println(book);
+        for (String b : books) {
+            out.println(b);
+        }
     }
 
     @Override
---
+++ mini-mvc/src/main/webapp/result.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhongpu
+  Date: 2022/1/2
+  Time: 22:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+
+</body>
+</html>

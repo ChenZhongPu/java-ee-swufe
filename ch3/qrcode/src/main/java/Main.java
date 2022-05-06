95823c1e7314b0ecdc21a38699b26686342df947
CHEN zhongpu <chenloveit@gmail.com>
2022-05-06 19:46:01 +0800

    add aliyun gradle for qrcode

--- ch3/qrcode/src/main/java/Main.java
+++ ch3/qrcode/src/main/java/Main.java
@@ -4,7 +4,6 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
 
